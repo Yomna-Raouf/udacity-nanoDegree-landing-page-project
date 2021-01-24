9e9ec26d03893a3c073c1d92264df4b76ebdc692
yomna raouf <yomnaraouf99@gmail.com>
2021-01-23 22:41:39 -0800

    built navBar - dynamically

---
+++ js/app.js
@@ -0,0 +1,83 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+
+const sections = document.querySelectorAll("section");
+const navList = document.querySelector("#navbar__list");
+const navLinks = document.querySelectorAll(".menu__link");
+
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+sections.forEach((section) => {
+  const navListItem = `
+    <li 
+        class='${section.className}' 
+        data-link=${section.id}
+    >
+        <a class='menu__link'  href="#${section.id}">
+            ${section.dataset.nav}
+        </a>
+    </li>`;
+
+  navList.insertAdjacentHTML("beforeend", navListItem);
+});
+
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+const scrollToAnchor = (navLinks) => {
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      scrollTo(link.getAttribute("name"), e, { behavior: "smooth" });
+    });
+  });
+};
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+// scrollToAnchor(navLinks);
+
+// Set sections as active
+
+
